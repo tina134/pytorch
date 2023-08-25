tina134/pytorch

commit 4a15bb0003fe7a2d1ca87ed1ed1f642da34053ed

Fix copy_ broadcast behavior on mps (#105617)
Fixes #105277

Pull Request resolved: https://github.com/pytorch/pytorch/pull/105617
Approved by: https://github.com/malfet

aten/src/ATen/native/mps/operations/Copy.mm
@@ -20,7 +20,7 @@ void* pageAlignedBlockPtr(const void* ptr, NSUInteger size, NSUInteger* alignedB
   return (void*)alignedAddress;
 }
 
-// Copy sourceBuffer into destBuffer, casting sourceBuffer to src.scalar_type().
+// Copy sourceBuffer into destBuffer, casting sourceBuffer to dst.scalar_type().
 // The shapes and dtypes are taken from dst and src, but their storage pointers are not used.
 void copy_cast_mps(at::Tensor& dst,
                    const at::Tensor& src,
@@ -290,8 +290,19 @@ at::Tensor& mps_copy_(at::Tensor& dst, const at::Tensor& src, bool non_blocking)
   if (dst.numel() == 0) {
     dst.resize_as_(src);
   }
+
+  TORCH_CHECK(dst.dim() >= src.dim());
   if (dst.dim() > src.dim()) {
     needs_broadcasting = true;
+  } else {
+    const IntArrayRef src_sizes = src.sizes();
+    const IntArrayRef dst_sizes = dst.sizes();
+    for (const auto j : c10::irange(src.dim())) {
+      if (src_sizes[j] == 1 && dst_sizes[j] != 1) {
+        needs_broadcasting = true;
+        break;
+      }
+    }
   }
 
   if (src.device().type() == at::kMPS && dst.device().type() == at::kCPU) {

test/test_mps.py
@@ -3545,6 +3545,30 @@ class TestMPS(TestCaseMPS):
         x_mps[2:4] = update_mps  # implicit type casting and copy
         self.assertEqual(x_cpu, x_mps)
 
+    def test_copy_broadcasting(self):
+        def helper(src_shape, dst_shape, src_dtype, dst_dtype):
+            cpu_src = torch.randint(0, 127, src_shape).to(src_dtype)
+            cpu_dst = torch.randint(0, 127, dst_shape).to(dst_dtype)
+            cpu_result = cpu_dst.copy_(cpu_src)
+            mps_src = cpu_src.to("mps")
+            mps_dst = cpu_dst.to("mps")
+            mps_result = mps_dst.copy_(mps_src)
+            self.assertEqual(cpu_result, mps_result)
+
+        test_dtypes = [torch.float32, torch.int32, torch.int16, torch.int8]
+
+        for (src_dtype, dst_dtype) in itertools.product(test_dtypes, test_dtypes):
+            helper((2, 1), (2, 3), src_dtype, dst_dtype)
+            helper((2, 1), (2, 2), src_dtype, dst_dtype)
+            helper((3, 1, 4, 1), (3, 4, 4, 5), src_dtype, dst_dtype)
+            helper((3,), (2, 3), src_dtype, dst_dtype)
+            helper((2,), (2, 2), src_dtype, dst_dtype)
+            helper((4, 1, 5), (3, 4, 4, 5), src_dtype, dst_dtype)
+            helper((4, 1, 5), (4, 0, 5), src_dtype, dst_dtype)
+            helper((1, 5), (4, 0, 5), src_dtype, dst_dtype)
+            helper((3, 1, 0), (3, 5, 0), src_dtype, dst_dtype)
+            helper((0, 1, 0), (0, 5, 0), src_dtype, dst_dtype)
+
     # See https://github.com/pytorch/pytorch/pull/84742
     # and https://github.com/pytorch/pytorch/pull/78319
     def test_binops_dtype_precedence(self):

commit 626c05307f011e869d17dd96f7e277c795f5e713

[MPS] Fix `.item()` for multi-dim scalar
By allowing to copy from tensor with multiple dimentions to tensor with
fewer dimentions, as long as destination size is 1

This fixes regression introduced by https://github.com/pytorch/pytorch/pull/105617 and adds regression test

copilot:poem

Fixes https://github.com/pytorch/pytorch/issues/107867

aten/src/ATen/native/mps/operations/Copy.mm
@@ -293,10 +293,9 @@ at::Tensor& mps_copy_(at::Tensor& dst, const at::Tensor& src, bool non_blocking)
     dst.resize_as_(src);
   }
 
-  TORCH_CHECK(dst.dim() >= src.dim());
   if (dst.dim() > src.dim()) {
     needs_broadcasting = true;
-  } else {
+  } else if (dst.dim() == src.dim()) {
     const IntArrayRef src_sizes = src.sizes();
     const IntArrayRef dst_sizes = dst.sizes();
     for (const auto j : c10::irange(src.dim())) {
@@ -305,6 +304,9 @@ at::Tensor& mps_copy_(at::Tensor& dst, const at::Tensor& src, bool non_blocking)
         break;
       }
     }
+  } else {
+    // dst.dim() can be smaller than src.dim() when scalars are copied
+    TORCH_CHECK(dst.numel() == 1, "Destination has ", dst.dim(), " dims, which is fewer than src ", src.dim());
   }
 
   if (src.device().type() == at::kMPS && dst.device().type() == at::kCPU) {

test/test_mps.py
@@ -3567,6 +3567,8 @@ class TestMPS(TestCaseMPS):
             helper((1, 5), (4, 0, 5), src_dtype, dst_dtype)
             helper((3, 1, 0), (3, 5, 0), src_dtype, dst_dtype)
             helper((0, 1, 0), (0, 5, 0), src_dtype, dst_dtype)
+        # Regression test for https://github.com/pytorch/pytorch/issues/107867
+        self.assertEqual(torch.tensor([[1]], device='mps').item(), 1.0)
 
     # See https://github.com/pytorch/pytorch/pull/84742
     # and https://github.com/pytorch/pytorch/pull/78319